0c0cddac7d35037a41c57959a062cec8c7fd284d
l0st <barrberry666@gmail.com>
2023-11-28 00:40:11 +0500

    On branch master Changes to be committed: 	new file:   fixtures/dump.json 	modified:   main_app/admin.py 	new file:   main_app/templates/main_page.html 	modified:   main_app/views.py 	modified:   to_do_app/urls.py
    DB dump added, main page for the site added, scripts modified

--- source/main_app/admin.py
+++ source/main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from main_app.models import Task
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'task_status', 'date']
+
+
+admin.site.register(Task, TaskAdmin)
--- source/main_app/views.py
+++ source/main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from main_app.models import Task
 # Create your views here.
+
+
+def main_list(request):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    print(type(tasks))
+    return render(request, 'main_page.html', context)
