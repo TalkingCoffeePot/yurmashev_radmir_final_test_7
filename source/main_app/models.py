a353260ba21075cb9cff3d7404cc603bb6b48443
l0st <barrberry666@gmail.com>
2023-11-27 23:54:35 +0500

    On branch master Changes to be committed: 	new file:   ../.gitignore 	new file:   main_app/migrations/0001_initial.py 	modified:   main_app/models.py 	modified:   to_do_app/settings.py
    Make migrations, plus created SU for admin rights

--- source/main_app/models.py
+++ source/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+class Task(models.Model):
+    description = models.TextField(max_length=500, null=False, blank=False, verbose_name='Описание')
+    task_status = models.CharField(max_length=50, null=False, blank=False, verbose_name='Текущий статус', choices=status_choices, default=status_choices[0])
+    date = models.CharField(max_length=10, null=False, blank=True, verbose_name='Сделать до...', default='yyyy-mm-dd')
+
+    def __str__(self):
+        return f'{self.pk}, {self.description}, {self.task_status}, {self.date}' 
