2552e4ea9a9e00670f9d3ce3ef2fe41777c836f2
l0st <barrberry666@gmail.com>
2023-11-30 23:35:18 +0500

    On branch homework_53  Changes to be committed: 	new file:   main_app/migrations/0003_alter_task_description_alter_task_details.py 	modified:   main_app/models.py
        Изменен тип текстовых полей Описание и Details

--- source/main_app/models.py
+++ source/main_app/models.py
@@ -5,10 +5,10 @@ from django.db import models
 status_choices = [('new', 'New'), ('in_progress', 'In progress'),  ('done', 'Done')]
 
 class Task(models.Model):
-    description = models.TextField(max_length=500, null=False, blank=False, verbose_name='Описание')
+    description = models.CharField(max_length=100, null=False, blank=False, verbose_name='Описание')
     task_status = models.CharField(max_length=50, null=False, blank=False, verbose_name='Текущий статус', choices=status_choices, default=status_choices[0])
     date = models.CharField(max_length=10, null=False, blank=True, verbose_name='Сделать до...', default='yyyy-mm-dd')
-    details = models.CharField(max_length=1500, null=False, blank=True)
+    details = models.TextField(max_length=1500, null=False, blank=True)
 
     def __str__(self):
         return f'{self.pk}, {self.description}, {self.task_status}, {self.date}' 
