5358a07425445829252a412e56550ab8e9bbdbb9
l0st <barrberry666@gmail.com>
2023-12-09 15:35:33 +0500

    Changes to be committed: 	modified:   main_app/static/css/main_style.css     Изменение стиля:     -исправлена верстка иконок, имени и почты

--- source/main_app/forms.py
+++ source/main_app/forms.py
@@ -4,6 +4,6 @@ from django.forms import widgets
 class GuestCardForm(forms.Form):
     name = forms.CharField(label='Имя автора', max_length=100, required=True)
     mail = forms.EmailField(label='Эл. почта', max_length=200, required=True)
-    description = forms.CharField(label='Детали задачи', max_length=1500, required=True, widget=widgets.Textarea)
+    description = forms.CharField(label='Детали записи', max_length=1500, required=True, widget=widgets.Textarea)
 
         
