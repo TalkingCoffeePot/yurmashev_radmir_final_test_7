153899ab1f675f13972489e9704654f124edbf6f
l0st <barrberry666@gmail.com>
2023-12-09 15:59:24 +0500

    Changes to be committed: 	modified:   main_app/forms.py     Исправлен заголовок формы для поля имени

--- source/main_app/forms.py
+++ source/main_app/forms.py
@@ -5,5 +5,3 @@ class GuestCardForm(forms.Form):
     name = forms.CharField(label='Имя автора', max_length=100, required=True)
     mail = forms.EmailField(label='Эл. почта', max_length=200, required=True)
     description = forms.CharField(label='Детали записи', max_length=1500, required=True, widget=widgets.Textarea)
-
-        
